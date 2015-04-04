a2dec72a0910db76e7dd000cd8cd22d343527230
James Patrick-Evans <james@jmpe.me>
2015-04-04 19:07:02 +0000

    add stop words, new corups and try to make topics more sparse

--- example/extract_topics.js
+++ example/extract_topics.js
@@ -1,13 +1,24 @@
 var fs = require('fs');
 var Matilda = require('../src/Matilda.js');
 
+
+
+var topics = 5; // number of topics
+var docs = -1; //limit to reading number of docs in corpus
+var iterations = 100; //number of iterations of lda
+
+
 var mM = new Matilda.Model();
-mM.setNumberOfTopics(3);
+mM.setNumberOfTopics(topics);
 
-//sync
-// var obj = JSON.parse(fs.readFileSync('file', 'utf8'));
 
-fs.readFile('internet.linux.computers.pretty.json', 'utf8', function(err, data){
+var stop_words_txt = fs.readFileSync('stop_words.json', 'utf8');
+var stop_words = JSON.parse(stop_words_txt);
+// fs.writeFileSync("stop_words.v.json", stop_words_txt.replace(/'/g, "\""));
+
+
+// fs.readFile('internet.linux.computers.pretty.json', 'utf8', function(err, data){
+fs.readFile('sports.arts.computers.buisness.management.home.society.science.json', 'utf8', function(err, data){
     if(err) throw err;
 
     var documents = JSON.parse(data);
@@ -15,20 +26,25 @@ fs.readFile('internet.linux.computers.pretty.json', 'utf8', function(err, data){
     documents.forEach(function(doc, docIndex){
         var wordlist = [];
 
-        if(docIndex > 50) return false;
+        if(docs > 0 && docIndex > docs) return false;
 
         //convert keys of dict to words placed as many time as seen
         for( var prop in doc.wordcounts){
+            if(stop_words.indexOf(prop) == -1) //if not is stop words
+            if(!prop.match(/\s/g)) //if not contain whitespace
+            if(!prop.match(/\d/g)) //if not contains digits
             if( doc.wordcounts.hasOwnProperty(prop) ){
+                // console.log(prop);
                 for(var i = 0; i < doc.wordcounts[prop]; ++i){
                     wordlist.push(prop);
                 }
             }
         }
+        // console.log('Wordlist size: ', wordlist.length);
         mM.addDocument(wordlist);
     });
 
-    mM.train(50, function(modelData){
+    mM.train(iterations, function(modelData){
     process.stdout.write('.');
         // console.log(modelData.vocab);
         // console.log(modelData.topics);
@@ -42,16 +58,49 @@ fs.readFile('internet.linux.computers.pretty.json', 'utf8', function(err, data){
     // console.log( mM.getVocabulary() );
 
 
-    mM.getWordsByTopics().forEach(function(topic, topicIndex){
-        //pick first N values and print them out
-        var N = 10;
-        var top10 = [];
-        for(var i = 0; i < N; ++i){
-            top10[i] = topic[i];
+    var topics = mM.getTopics();
+    topics.forEach(function(topic, topicIndex){
+        //find and print highest 10 wordcounts
+        var highest = [];
+        for(var prop in topic.withWord){
+            if(topic.withWord.hasOwnProperty(prop)){
+                if(highest.length < 10){
+                    highest.push( [ prop, topic.withWord[prop] ] );
+                    // console.log("Pushing word ", prop, " with wordcount ", topic.withWord[prop]);
+                    continue;
+                }
+
+                //get min wordcount from highest arr
+                var min = Math.min.apply(null, highest.map(function(x){ return x[1]}) );
+                var minInd = highest.map(function(x){ return x[1]}).indexOf(min);
+                if(minInd == -1){
+                    console.error("No index for highest with wordcount: ", min);
+                    return;
+                }
+
+                if(topic.withWord[prop] > min){
+                    //add prop to highest
+                    highest[minInd] = [ prop, topic.withWord[prop] ];
+                }
+            }
         }
-        console.log('Topic: ', topicIndex);
-        console.log(top10);
+        console.log("Topic: ", topic.id);
+        console.log(highest.sort(function(a, b){
+            return a[1] < b[1];
+        }));
     });
+
+
+    // mM.getWordsByTopics().forEach(function(topic, topicIndex){
+    //     //pick first N values and print them out
+    //     var N = 10;
+    //     var top10 = [];
+    //     for(var i = 0; i < N; ++i){
+    //         top10[i] = topic[i];
+    //     }
+    //     console.log('Topic: ', topicIndex);
+    //     console.log(top10);
+    // });
 
 });
 
